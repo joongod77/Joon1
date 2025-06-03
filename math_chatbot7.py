3b236f943b5b14fcfb1daf6682568fde46e925cf
joongod77 <dlehdwns7797@naver.com>
2025-06-03 14:07:09 +0900

    Update math_chatbot7.py

--- math_chatbot7.py
+++ math_chatbot7.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.title("수학 과외 챗봇")
 
 # 비밀 키 불러오기
-api_key = st.secrets["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"]
+api_key = st.secrets["OPENAI_API_KEY"]
 
 # client에 키 명시적으로 전달
 client = openai.OpenAI(api_key=api_key)
